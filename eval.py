47b5542df88395196e31823df85777028638e884
Nawaz393 <mohdnawaz6393@gmail.com>
2024-02-20 20:18:42 +0500

    ..

--- eval.py
+++ eval.py
@@ -7,9 +7,12 @@ import torch_xla
 import torch_xla.core.xla_model as xm
 
 import os
+
+
 def single_image_inference(image_pth, model_pth, mask_pth, device):
     model = UNet(in_channels=1, num_classes=1).to(device)
-    model.load_state_dict(torch.load(model_pth, map_location=torch.device(device)))
+    model.load_state_dict(torch.load(
+        model_pth, map_location=torch.device(device)))
     model.eval()
 
     transform = transforms.Compose([
@@ -21,13 +24,12 @@ def single_image_inference(image_pth, model_pth, mask_pth, device):
     img = img.unsqueeze(0)
 
     with torch.no_grad():
-        pred_mask = model(img) 
-        
+        pred_mask = model(img)
+
     img = img.squeeze(0).cpu().permute(1, 2, 0)
     pred_mask = pred_mask.squeeze(0).cpu().permute(1, 2, 0)
-    pred_mask[pred_mask< 0] = 0
+    pred_mask[pred_mask < 0] = 0
     pred_mask[pred_mask > 0] = 1
-    
 
     fig, axes = plt.subplots(1, 3, figsize=(12, 4))
     axes[0].imshow(img, cmap="gray")
@@ -39,14 +41,16 @@ def single_image_inference(image_pth, model_pth, mask_pth, device):
     axes[2].imshow(Image.open(mask_pth), cmap="gray")
     axes[2].set_title("Ground Truth Mask")
 
-    plt.show()
+    plt.show(block=True)
     return pred_mask.numpy()
 
+
 if __name__ == "__main__":
-    num=101215
+    num = 101215
     SINGLE_IMG_PATH = f"../SpinePatchesDataset1/data/data_patch_{num}.png"
     MODEL_PATH = "./models/Single_SpineSegmentationv6.pth"
     MASK_PATH = f"../SpinePatchesDataset1/label/label_patch_{num}.png"
-    device=xm.xla_device()
+    device = xm.xla_device()
 
-    pred_mask=single_image_inference(SINGLE_IMG_PATH, MODEL_PATH, MASK_PATH, device)
+    pred_mask = single_image_inference(
+        SINGLE_IMG_PATH, MODEL_PATH, MASK_PATH, device)
